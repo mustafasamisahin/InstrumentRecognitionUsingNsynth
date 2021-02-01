a8c0bdc6cd62aa236427088a3632488cf3234706
mustafasamisahin <mustafa_sami95@hotmail.com>
2021-02-01 15:27:13 +0300

    Rapor düzenleme + enstruman ayıklama

--- InstrumentRecognition.py
+++ InstrumentRecognition.py
@@ -14,23 +14,27 @@ def main():
     train_columns.extend(qualities_columns)
     train_columns.append('instrument_family')
     
+    unwanted_ins = ["string", "reed", "brass", "vocal","flute", "synth_lead"]
+    
     
     df_train_raw = pd.read_json(path_or_buf='train.json', orient='index')
-    df_train = df_train_raw.drop(unwanted_columns, axis=1)
-    df_train = df_train.join(pd.DataFrame(df_train.pop('qualities').values.tolist(), index=df_train.index, columns=qualities_columns))
+    df_train_raw = df_train_raw[~df_train_raw["instrument_family_str"].isin(unwanted_ins)]
+    df_train = df_train_raw.join(pd.DataFrame(df_train_raw.pop('qualities').values.tolist(), index=df_train_raw.index, columns=qualities_columns))
+    df_train = df_train.drop(unwanted_columns, axis=1)
     df_train = df_train[train_columns]
 
         
     df_test_raw = pd.read_json(path_or_buf='test.json', orient='index')
-    df_test = df_test_raw.drop(unwanted_columns, axis=1)
-    df_test = df_test.join(pd.DataFrame(df_test.pop('qualities').values.tolist(), index = df_test.index, columns=qualities_columns))
+    df_test_raw = df_test_raw[~df_test_raw["instrument_family_str"].isin(unwanted_ins)]
+    df_test = df_test_raw.join(pd.DataFrame(df_test_raw.pop('qualities').values.tolist(), index = df_test_raw.index, columns=qualities_columns))
+    df_test = df_test.drop(unwanted_columns, axis=1)
     df_test = df_test[train_columns]
     
     X = df_train[train_columns[:-1]]
         
     y = df_train['instrument_family']
     
-    model = KNeighborsClassifier(n_neighbors=13)
+    model = KNeighborsClassifier(n_neighbors=3)
         
     model.fit(X,y)
     result_family = model.predict(df_test.loc[:, df_test.columns != 'instrument_family'])
